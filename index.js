7b97468db929d8726cc69e89b1fd1982d178bce7
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-06-16 20:41:25 +0600

    save score in db

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 
 
-const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2'
+// const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2'
+const uri =  `mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.0`
 
 // mongodb
 const client = new MongoClient(uri, {
@@ -30,6 +31,16 @@ async function run() {
  
     await client.connect();
     const quizCategoryCollection = client.db('quizDB').collection('category')
+    const quizCollection = client.db('quizDB').collection('quiz')
+    const usersCollection = client.db('quizDB').collection('users')
+    const scoreCollection = client.db('quizDB').collection('score')
+
+    // save user in db
+    app.post('/user', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
 
     // save quiz category in db
     app.post('/add-category', async(req, res) => {
@@ -42,6 +53,27 @@ async function run() {
     app.get('/quiz-category', async(req,res) => {
       const result = await quizCategoryCollection.find().toArray();
       res.send(result)
+    })
+
+    // save quiz to db
+    app.post('/add-quiz', async(req, res) => {
+      const categoryData = req.body;
+      const result = await quizCollection.insertOne(categoryData);
+      res.send(result)
+    })
+
+    // get quiz from db
+    app.get('/quiz', async(req,res) => {
+      const category = req.query.name;
+      const query = {quizCategory: category}
+      const result = await quizCollection.find(query).toArray();
+      res.send(result)
+    })
+    // save quiz score 
+    app.post('/quiz-score', async(req, res) => {
+      const body = req.body;
+      const result = await scoreCollection.insertOne(body)
+      res.send(result);
     })
     
     await client.db("admin").command({ ping: 1 });
